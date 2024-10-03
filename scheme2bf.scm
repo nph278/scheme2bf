17a7c616d4173b7fe4fb27b50a724f979eb5eb26
nph278 <carllegrone@protonmail.com>
2024-10-03 19:29:38 -0400

    clean up

--- scheme2bf.scm
+++ scheme2bf.scm
@@ -1,14 +1,10 @@
-(use-modules (srfi srfi-1)
-             (srfi srfi-26))
-
+(use-modules (srfi srfi-1))
 
 ;; COMPILER
-
 
 (define (compile l0)
   (let* ((bf (l0s->bf l0)))
     bf))
-
 
 ;; L0: SIMPLE BYTE TAPE
 ;;
@@ -18,8 +14,18 @@
 ;; reset-up     -- Reset current cell to 0. More performant on higher (>127) cells.
 ;; loop . exprs -- Loop expressions while current cell is not 0.
 
-
 (define (l0->bf expr)
+  (define (bf-number n)
+    (let ((n (modulo n 256)))
+      (if (< n 128)
+          (make-string n #\+)
+          (make-string (- 256 n) #\-))))
+
+  (define (bf-move n)
+    (if (> n 0)
+        (make-string n #\>)
+        (make-string (- n) #\<)))
+
   (cond ((not (list? expr)) (error "l0 ERROR: non-list instruction"))
         ((nil? expr) (error "l0 ERROR: nil instruction"))
         (else (case (first expr)
@@ -42,21 +48,7 @@
 (define (l0s->bf exprs)
   (apply string-append (map l0->bf exprs)))
 
-
-(define (bf-number n)
-  (let ((n (modulo n 256)))
-    (if (< n 128)
-        (make-string n #\+)
-        (make-string (- 256 n) #\-))))
-
-(define (bf-move n)
-  (if (> n 0)
-      (make-string n #\>)
-      (make-string (- n) #\<)))
-
-
 ;; CLI
-
 
 (define example '((add 254) (move 10) (move -10) (reset) (reset-up)))
 (display (compile example))
